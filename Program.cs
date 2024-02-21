1df3b678aba55fa305ecc1e13815ed69bcf18db2
Melanie Badt <melanie@Melanies-MacBook-Air.local>
2024-02-21 23:56:35 +0100

    Task 1 completed

--- Program.cs
+++ Program.cs
@@ -7,21 +7,41 @@ Console.Clear();
 Console.WriteLine("Starting Assignment 2");
 
 // SETUP 
-const string myPersonalID = ""; // GET YOUR PERSONAL ID FROM THE ASSIGNMENT PAGE https://mm-203-module-2-server.onrender.com/
+const string myPersonalID = "c205b98b832782cc70a8b178b224a6419cd30a782b297c818aae923f74141018";
 const string baseURL = "https://mm-203-module-2-server.onrender.com/";
-const string startEndpoint = "start/"; // baseURl + startEndpoint + myPersonalID
-const string taskEndpoint = "task/";   // baseURl + taskEndpoint + myPersonalID + "/" + taskID
+const string startEndpoint = "start/";
+const string taskEndpoint = "task/";
 
-// Creating a variable for the HttpUtils so that we dont have to type HttpUtils.instance every time we want to use it
 HttpUtils httpUtils = HttpUtils.instance;
 
 //#### REGISTRATION
-// We start by registering and getting the first task
-Response startResponse = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
-Console.WriteLine($"Start:\n{Colors.Magenta}{startResponse}{ANSICodes.Reset}\n\n"); // Print the response from the server to the console
-string taskID = ""; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
+Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
+Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n");
+string taskID = "aAaa23";
 
 //#### FIRST TASK 
-// Fetch the details of the task from the server.
-Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
+Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID);
 Console.WriteLine(task1Response);
+Task task1 = JsonSerializer.Deserialize<Task>(task1Response.content);
+
+double temperatureInFahrenheit = double.Parse(task1.parameters);
+double temperatureInCelsius = (temperatureInFahrenheit - 32) / 1.8;
+string answer = temperatureInCelsius.ToString("0.00");
+
+Response task1AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answer);
+Console.WriteLine($"Fahrenheit:{task1.parameters}; Celsius:{answer}");
+Console.WriteLine($"Answer: {Colors.Green}{task1AnswerResponse}{ANSICodes.Reset}");
+
+
+taskID = "psu31_4";
+
+Console.WriteLine("\n-----------------------------------\n");
+
+class Task
+{
+    public string? title { get; set; }
+    public string? description { get; set; }
+    public string? taskID { get; set; }
+    public string? usierID { get; set; }
+    public string? parameters { get; set; }
+}
